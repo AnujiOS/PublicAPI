a3a26dd7021c2804125f532c01aceef8b83fb10b
anujjoshi <anuj@groktor.net>
2021-04-30 13:41:15 +0530

    Add Spinner for API call

--- PublicAPI/PublicAPI/Controller/Category/CategoriesViewController.swift
+++ PublicAPI/PublicAPI/Controller/Category/CategoriesViewController.swift
@@ -50,20 +50,20 @@ class CategoriesViewController: UIViewController {
     }
     func showLoader(view: UIView) -> UIActivityIndicatorView {
 
-            //Customize as per your need
-            let spinner = UIActivityIndicatorView(frame: CGRect(x: 0, y: 0, width: 60, height:60))
-            spinner.backgroundColor = UIColor.black.withAlphaComponent(0.7)
-            spinner.layer.cornerRadius = 3.0
-            spinner.clipsToBounds = true
-            spinner.hidesWhenStopped = true
-            spinner.style = UIActivityIndicatorView.Style.medium
-            spinner.center = view.center
-            view.addSubview(spinner)
-            spinner.startAnimating()
-            UIApplication.shared.beginIgnoringInteractionEvents()
+        //Customize as per your need
+        let spinner = UIActivityIndicatorView(frame: CGRect(x: 0, y: 0, width: 60, height:60))
+        spinner.backgroundColor = UIColor.black.withAlphaComponent(0.7)
+        spinner.layer.cornerRadius = 3.0
+        spinner.clipsToBounds = true
+        spinner.hidesWhenStopped = true
+        spinner.style = UIActivityIndicatorView.Style.medium
+        spinner.center = view.center
+        view.addSubview(spinner)
+        spinner.startAnimating()
+        UIApplication.shared.beginIgnoringInteractionEvents()
 
-            return spinner
-        }
+        return spinner
+    }
 }
 extension CategoriesViewController: UITableViewDelegate, UITableViewDataSource {
 
